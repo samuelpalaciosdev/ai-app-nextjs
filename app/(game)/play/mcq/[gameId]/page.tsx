b0e848609adb94b2fca952c9e7293d650ebb8e68
Samuel Palacios <samuelpl0888@gmail.com>
2023-09-01 00:58:00 -0400

    Add game pages mockups and add dark mode styling for user dropdown

---
+++ app/(game)/play/mcq/[gameId]/page.tsx
@@ -0,0 +1,12 @@
+export default function McqGamePage() {
+  return (
+    <>
+      <div className='container flex flex-col max-w-[64rem] mx-auto gap-4 text-center'>
+        <h1 className='text-4xl lg:text-7xl font-semibold'>MCQ :)</h1>
+        <p className='max-w[42rem] text-muted-foreground sm:text-xl sm:leading-8'>
+          Here is the Mcq game page
+        </p>
+      </div>
+    </>
+  );
+}
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -25,12 +25,12 @@ const UserAccountNav = ({ user }: Props) => {
         {/* User image */}
         <UserAvatar user={user} />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='bg-white' align='end'>
+      <DropdownMenuContent className='bg-white dark:bg-gray-950' align='end'>
         <div className='flex items-center justify-start gap-2 p-2'>
           <div className='flex flex-col space-y-1 leading-none'>
             {user.name && <p className='font-medium'>{user.name}</p>}
             {user.email && (
-              <p className='w-[200px] truncate text-sm text-zinc-700'>
+              <p className='w-[200px] truncate text-sm text-zinc-700 dark:text-white'>
                 {user.email}
               </p>
             )}
